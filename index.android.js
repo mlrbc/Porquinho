d39c1ef2412d668436c271bca437888c33b1ba50
Moabe Renato <mrov@cin.ufpe.br>
2017-05-20 19:45:48 -0300

    Tela de Registrar compra
    Todo trabalho feito até dia 20/05, ainda falta fazer a barra inferior de icones e o pop-up que aparece depois de registrar compra com as informações do registro da compra.

--- index.android.js
+++ index.android.js
@@ -9,24 +9,59 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
 } from 'react-native';
 
 export default class Porquin extends Component {
   render() {
     return (
+
+
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+
+        <View style={styles.header}>
+          <View style={styles.headerTextBox}>
+            <Text style={styles.headerText}>Registro de Compras</Text>
+          </View>
+          <View style={styles.subBarra}>
+            <Text style={styles.subBotoesText}>INSERIR</Text>
+            <Text style={styles.subBotoesText}>RETIRAR</Text>
+            <Text style={styles.subBotoesText}></Text>
+          </View>
+        </View>
+
+        <View style={styles.centroContainer}>
+          <Text style={styles.labelcpf}>Digite o CPF do comprador:</Text>
+          <View style={styles.barrainput1}>
+            <TextInput style={{fontSize:22,textAlign:'center',fontWeight: 'bold'}}
+            onChangeText={(text)=>this.setState({text})}
+             placeholder={'CPF'}>
+            </TextInput>
+          </View>
+          <Text style={styles.labelcompra}>Digite o valor da compra:</Text>
+          <View style={styles.barrainput2}>
+            <TextInput style={{fontSize:22,textAlign:'center',color:'green',fontWeight: 'bold'}}
+            onChangeText={(text)=>this.setState({text})}
+            placeholder={'Valor'}>
+            </TextInput>
+          </View>
+        </View>
+
+        <TouchableOpacity
+        onPress={() => {alert('era para o popup aparecer aqui')}}
+        style={styles.botaoregistrar} >
+          <Text style={{fontSize:16,color:'white',top: 2, fontFamily:'System'}}>REGISTRAR</Text>
+        </TouchableOpacity>
+
+        <View style={styles.footer}>
+          <Text style={{fontSize: 30}}> Aqui será a barra de icones </Text>
+        </View>
+
       </View>
+
     );
   }
 }
@@ -34,19 +69,90 @@ export default class Porquin extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+  },
+  header: {
+    flex:0.55,
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: 'column',
+    backgroundColor: '#607D8B',
+
   },
-  welcome: {
+  headerTextBox:{
+    flex: 1.50,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  headerText:{
+    paddingTop: 25,
+    color: 'white',
+    fontSize: 24,
+  },
+  subBarra:{
+    flex:1,
+    flexDirection: 'row',
+    paddingRight: 22,
+    width: 410,
+  },
+  subBotoes:{
+
+
+  },
+  subBotoesText:{
+    flex: 1,
+    fontSize: 16,
+    letterSpacing: 1,
+    fontFamily:'System',
+    color: 'white',
+    alignSelf: 'center',
+    textAlign: 'center',
+  },
+  centroContainer:{
+    flex:2.50,
+  },
+  labelcpf:{
+    top: 30,
+    left: 30,
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    color: '#808080',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  labelcompra:{
+    top: 60,
+    left: 30,
+    fontSize: 20,
+    color: '#808080',
+  },
+  barrainput1:{
+    position: 'relative',
+    left: 50,
+    top: 42,
+    borderRadius: 100/2,
+    width: 300,
+    height: 45,
+    backgroundColor: '#d9d9d9',
+  },
+  barrainput2:{
+    position: 'relative',
+    left: 50,
+    top: 80,
+    borderRadius: 100/2,
+    width: 300,
+    height: 45,
+    backgroundColor: '#d9d9d9',
+  },
+  botaoregistrar:{
+    position: 'absolute',
+    alignItems: 'center',
+    alignSelf: 'center',
+    borderRadius: 100/2,
+    top: 400,
+    width: 130,
+    height: 30,
+    backgroundColor: '#60B293',
+  },
+  footer:{
+    flex: 0.30,
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 });
 
